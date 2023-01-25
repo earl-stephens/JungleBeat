a0e236fe1767b88e688b19fe9eb5481cc2a65128
earl-stephens <stephensearl@hotmail.com>
2023-01-25 15:54:05 -0700

    Add user input for voice rate

--- src/main/JungleBeat.java
+++ src/main/JungleBeat.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class JungleBeat {
 	public LinkedList list;
@@ -19,11 +20,27 @@ public class JungleBeat {
 	}
 	
 	public void play() {
-		String command = "say " + list.to_string();
+		String rateToPlay = getRate();
+		String command = "say -r " + rateToPlay + list.to_string();
 		try {
 		Process process = Runtime.getRuntime().exec(command);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private String getRate() {
+		String rate = "";
+		System.out.println("Please enter a number between 100 and 500 for the voice rate.");
+		System.out.println("Or enter the word rate for the default voice rate.");
+		Scanner scanner = new Scanner(System.in);
+		rate = scanner.next();
+		scanner.close();
+		
+		if(rate.equals("rate")) {
+			rate = "500";
+		} 
+		
+		return rate;
 	}
 }
